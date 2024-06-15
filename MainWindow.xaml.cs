c518abb3886d1340016564c315f6413cd06cc234
Rezbirn <rezbirn@gmail.com>
2024-06-16 02:16:14 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INDZ_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string[] _checkBoxNames = { "one", "two", "three", "four", "five", "six" };
+        Window1 _window1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _window1 = new Window1(this);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            var tag = textBox.Tag;
+            if(tag != null) 
+            {
+                var index = int.Parse(tag.ToString());
+                var checkBoxFindByName = _window1.FindName(_checkBoxNames[index]);
+                if (checkBoxFindByName is CheckBox checkBox)
+                {
+                    checkBox.Content = textBox.Text;
+                }
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _window1.Owner = this;
+            _window1.ShowDialog();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INDZ_2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private static string[] _textBoxNames = { "one", "two", "three", "four", "five", "six" };
+        private MainWindow _mainWindow;
+        public Window1(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            var tag = checkBox.Tag;
+            if(tag!= null) 
+            {
+                var index = int.Parse(tag.ToString());
+                var name = _textBoxNames[index];
+                var textBoxFindByName = _mainWindow.FindName(name);
+                if (textBoxFindByName is TextBox textBox)
+                {
+                    if (checkBox.IsChecked == true)
+                        textBox.FontWeight = FontWeights.Bold;
+                    else
+                        textBox.FontWeight = FontWeights.Normal;
+                }
+            }
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+            e.Cancel = true;
+        }
+    }
+}
